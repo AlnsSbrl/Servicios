fcc2a92d695ca5f7eeb2fa11196efca0f8a383fc
alnssbrl <alonsosobralpablo@gmail.com>
2023-01-27 13:58:27 +0100

    creado el proyecto, la clase apenas tiene cuerpo

---
+++ ChatRoom/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Servidor s = new Servidor();
+            s.IniciaChatRoom();
+        }
+    }
+}
---
+++ ChatRoom/Servidor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatRoom
+{
+    internal class Servidor
+    {
+        Socket sServer;
+        string IP_SERVER;
+        int port;
+        int backUpPort;
+        public void IniciaChatRoom()
+        {
+            sServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IP_SERVER),port);
+
+            }catch(SocketException e) when (e.ErrorCode == (int)SocketError.AddressNotAvailable){
+
+            }
+        }
+    }
+}
